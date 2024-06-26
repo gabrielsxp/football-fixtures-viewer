d1991d6f3c622cd0a4e7127b24f53dae05d54f3b
Gabriel Costa <gabrielsxp@hotmail.com>
2024-06-26 17:40:03 -0300

    feat: adding Team

---
+++ src/components/Team/index.tsx
@@ -0,0 +1,35 @@
+import { Flex, Text, Tooltip } from "@radix-ui/themes";
+import Image from "next/image";
+import { IAwayTeam, IHomeTeam } from "../../../global";
+
+interface ITeamComponent {
+  data: IHomeTeam | IAwayTeam;
+  isWinner?: boolean;
+}
+
+const Team = ({ data, isWinner }: ITeamComponent) => {
+  return (
+    <Flex direction="column" justify="center" align="center" gap="2" p="2">
+      <Image
+        width={48}
+        height={48}
+        alt={`${data.name}'s crest`}
+        src={data.crest}
+        style={{ height: "auto" }}
+      />
+      <Flex maxWidth="70px">
+        <Tooltip delayDuration={200} content={data.name}>
+          <Text
+            style={{ cursor: "help" }}
+            truncate
+            color={isWinner ? "green" : "gray"}
+          >
+            {data.tla}
+          </Text>
+        </Tooltip>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Team;
